0ee9d8b1d71bda4281273e4bfbf39f4a39888c90
Sammuel Reis <sammugr@hotmail.com>
2026-01-14 02:12:24 -0300

    feat: create NotFound page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,7 +1,17 @@
+import { Link } from 'react-router';
+
+import { PATHS } from '@/routes/paths';
+
 export default function NotFound() {
   return (
-    <div>
-      <h1>Not found</h1>
+    <div className="flex flex-col min-h-screen items-center justify-center text-center bg-muted">
+      <h1 className="mb-4 text-4xl text-primary font-bold">404</h1>
+
+      <p className="mb-4 text-xl">Oops! Page not found</p>
+
+      <Link className="text-highlight underline hover:text-highlight/90" to={PATHS.GENERATOR}>
+        Return to Home
+      </Link>
     </div>
   );
 }
